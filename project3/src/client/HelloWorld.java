a8821c57e935b54df216b35f39941c96bbbec82e
krautz <caio_k@hotmail.com>
2019-06-13 11:45:51 -0300

    Use JSON example in helloworld

--- project3/src/client/HelloWorld.java
+++ project3/src/client/HelloWorld.java
@@ -1,18 +1,35 @@
 package client;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import compute.Task;
 import java.io.Serializable;
-import java.math.BigDecimal;
 
 public class HelloWorld implements Task<String>, Serializable {
 
     private static final long serialVersionUID = 227L;
-
-
+    
+    private String key;
+    
+    public HelloWorld (String key) {
+        this.key = key;
+    }
+    
     /**
      * Calculate pi.
      */
-    public String execute() {
-        return "Hello World !!!";
+    public String execute() throws JSONException {
+    	
+    	long start = System.currentTimeMillis();
+
+    	JSONObject my_obj = new JSONObject("{'0': { 'name': 'caio', 'cargo': 'estudante'}, '1': { 'name': 'allan', 'cargo': 'estudante2'}}");
+    	
+    	String name = my_obj.getJSONObject(key).getString("name");
+    	
+    	long end = System.currentTimeMillis();
+    	
+    	System.out.println("TEMPO SERVIDOR: " + (end - start));
+ 
+        return name;
     }
 }
